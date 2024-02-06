c072449bfc69d9f3cebf7b9d710b216de659e9a4
SimoneCamanitti <145446101+SimoneCamanitti@users.noreply.github.com>
2024-02-06 12:57:54 +0100

    changes

--- ecommerce/SQL/Inserimento dell'admin.sql
+++ ecommerce/SQL/Inserimento dell'admin.sql
@@ -1,2 +1,2 @@
 INSERT INTO utenti (Email,pasword_utente,tipo) VALUES 
-	("ssimone.camanitti@itis.pr.it","ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f",1);
+	("ssimone.camanitti@itis.pr.it","$2y$10$FZZzalEd/X0xYMNDptQll.DcKNWXahrBC7PP5rK6732kH0u.Esj1i",1);

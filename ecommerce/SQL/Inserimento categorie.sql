182dc646468994a3f2c901f55db7ca16b4b8d067
SimoneCamanitti <ssimone.camanitti@itis.pr.it>
2024-02-08 12:20:13 +0100

    c

---
+++ ecommerce/SQL/Inserimento categorie.sql
@@ -0,0 +1,9 @@
+INSERT INTO categorie (Nome) VALUES
+	("Divani"),
+	("Sedie"),
+	("Armadi"),
+	("Letti"),
+	("Illuminazione"),
+	("Giardino"),
+	("Cucina"),
+	("Bagno");
--- ecommerce/SQL/Inserimento dell'admin.sql
+++ ecommerce/SQL/Inserimento dell'admin.sql
@@ -1,2 +1,2 @@
-INSERT INTO utenti (Email,pasword_utente,tipo) VALUES 
+INSERT INTO utenti (Email,password_utente,tipo) VALUES 
 	("ssimone.camanitti@itis.pr.it","$2y$10$FZZzalEd/X0xYMNDptQll.DcKNWXahrBC7PP5rK6732kH0u.Esj1i",1);
